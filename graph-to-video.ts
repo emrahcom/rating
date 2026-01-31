326c15e37787c90f6be77758922fe2eab4a88865
emrah <emrah.eryilmaz@conceptboard.com>
2026-01-31 14:33:22 +0300

    episode 16

--- graph-to-video.ts
+++ graph-to-video.ts
@@ -6,16 +6,16 @@
 //   deno run --allow-run --allow-read --allow-write graph-to-video.ts <GRAPH>
 // -----------------------------------------------------------------------------
 // Pixel coordinate of the starting point (minus 1) on X axis, e.g. 120
-const X0 = 60;
+const X0 = 151;
 // Pixels per second, e.g. 0.150
 // pixels_difference_of_two_points / time_in_sec
-const PPS = (1737 - 92) / (3 * 3600);
+const PPS = (1900 - 135) / (3 * 3600);
 // Pixel coordinate of the top point of the slider on Y axis, e.g. 90
-const Y0 = 72;
+const Y0 = 79;
 // Pixel coordinate of the bottom point of the slider on Y axis, e.g. 1010
-const Y1 = 935;
+const Y1 = 921;
 // Length of the video (MP4 file) in second, e.g. 9112
-const SECONDS = 9814;
+const SECONDS = 8515;
 // Number of frames per second (default 0.5)
 const FRAMERATE = 0.5;
 
@@ -29,18 +29,18 @@ const FRAMERATE = 0.5;
 // - Update the codes in loop if the number of breaks is more than 4.
 
 // First break
-const SEC1 = 112;
-const ADV1 = 633;
+const SEC1 = 116;
+const ADV1 = 516;
 const PXL1 = PPS * ADV1;
 
 // Second break
-const SEC2 = 5723;
-const ADV2 = 807;
+const SEC2 = 4852;
+const ADV2 = 950;
 const PXL2 = PPS * ADV2;
 
 // Third break
-const SEC3 = 9720;
-const ADV3 = 720;
+const SEC3 = 8418;
+const ADV3 = 631;
 const PXL3 = PPS * ADV3;
 
 // Fourth break
