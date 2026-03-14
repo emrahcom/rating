6b55ffa4a697ea738a7d505c8f01098fbd9b0c34
emrah <emrah.eryilmaz@conceptboard.com>
2026-03-14 17:21:30 +0300

    episode 21

--- graph-to-video.ts
+++ graph-to-video.ts
@@ -6,14 +6,14 @@
 //   deno run --allow-run --allow-read --allow-write graph-to-video.ts <GRAPH>
 // -----------------------------------------------------------------------------
 // Pixel coordinate of the starting point (minus 1) on X axis, e.g. 120
-const X0 = 155;
+const X0 = 146;
 // Pixels per second, e.g. 0.150
 // pixels_difference_of_two_points / time_in_sec
-const PPS = (1708 - 73) / (3 * 3600);
+const PPS = (1720 - 121) / (3 * 3600);
 // Pixel coordinate of the top point of the slider on Y axis, e.g. 90
-const Y0 = 60;
+const Y0 = 116;
 // Pixel coordinate of the bottom point of the slider on Y axis, e.g. 1010
-const Y1 = 932;
+const Y1 = 912;
 // Length of the video (MP4 file) in second, e.g. 9112
 const SECONDS = 9550;
 // Number of frames per second (default 0.5)
